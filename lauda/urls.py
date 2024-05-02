50201fa10a3d81f1e72387ad0f95241180d68501
Cedric Bansah <cedibans@gmail.com>
2024-05-02 22:50:15 +0000

    to railway

--- lauda/urls.py
+++ lauda/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from lauda import views
 
 
-urlpatterns = [
-    # path('list_vehicles/', views.list_vehicles, name='list_vehicles'),
-    # path('vehicles_info/<int:pk>/', views.vehicle_info, name='vehicles_info')
-    # path('forgot_password/')
-]
+# urlpatterns = [
+#     # path('list_vehicles/', views.list_vehicles, name='list_vehicles'),
+#     # path('vehicles_info/<int:pk>/', views.vehicle_info, name='vehicles_info')
+#     # path('forgot_password/')
+# ]
